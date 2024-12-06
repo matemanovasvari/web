db5ff1471abad4fcdf3e57b94881416e69fc2c4f
Nagy Máté <nagy.mano.mate@gmail.com>
2024-12-06 21:46:16 +0100

    apesmn

--- menu/index.js
+++
@@ -1,8 +0,0 @@
-const list = document.querySelectorAll("#container>div");
-function activeMenu(){
-    list.forEach((item) => 
-        item.classList.remove("menu"));
-    this.classList.add("menu");
-}
-
-list
